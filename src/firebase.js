ce63e527281460023c79a2c76ca78f97a7aaf03c
cabanafernando1-byte <cabanafernando1@gmail.com>
2026-09-07 12:53:51 -0500

    con fire

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,14 @@
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyC5l7gQ1ooB-HLJgveOrEklgpjhrKNIKWo',
-  authDomain: 'cifra-nasa.firebaseapp.com',
-  projectId: 'cifra-nasa',
-  storageBucket: 'cifra-nasa.firebasestorage.app',
-  messagingSenderId: '843530351902',
-  appId: '1:843530351902:web:a54db61477a4b0c666050f',
+  apiKey: "AIzaSyC5l7Gq1ooB-HlJgveOrEklgpjhrKNikWo",
+  authDomain: "cifra-nasa.firebaseapp.com",
+  projectId: "cifra-nasa",
+  storageBucket: "cifra-nasa.firebasestorage.app",
+  messagingSenderId: "843530351902",
+  appId: "1:843530351902:web:a54db61477a4b0c666050f",
+  measurementId: "G-TKM9M2097K"
 };
 
 const app = initializeApp(firebaseConfig);
